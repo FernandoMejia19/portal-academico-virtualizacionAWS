1c740d42b4facb4dab3113d2e8f4f6ff360fc006
AngyUwU2005XD <angelicapanama2005@gmail.com>
2026-06-15 10:30:54 -0500

    Frontend Cambios

--- frontend/front-portal-academico/src/app/app.ts
+++ frontend/front-portal-academico/src/app/app.ts
@@ -1,12 +1,81 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ApiService } from './services/api.spec';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('front-portal-academico');
+
+  recursos: any[] = [];
+
+  nuevoUsuario = {
+    name: '',
+    email: ''
+  };
+
+  nuevoRecurso = {
+    title: '',
+    description: '',
+    resource_type: '',
+    url: '',
+    user_id: 1
+  };
+
+  constructor(private api: ApiService) {}
+
+  crearUsuario() {
+
+    this.api.createUser(this.nuevoUsuario)
+      .subscribe({
+        next: (resp: any) => {
+          console.log(resp);
+          alert('Usuario creado');
+        },
+        error: (err: any) => {
+          console.error(err);
+        }
+      });
+  }
+
+  crearRecurso() {
+
+    this.api.createResource(this.nuevoRecurso)
+      .subscribe({
+        next: (resp: any) => {
+          console.log(resp);
+          alert('Recurso creado');
+          this.cargarRecursos();
+        },
+        error: (err: any) => {
+          console.error(err);
+        }
+      });
+  }
+
+  cargarRecursos() {
+
+    this.api.getResources()
+      .subscribe({
+        next: (data: any) => {
+          this.recursos = data;
+        },
+        error: (err: any) => {
+          console.error(err);
+        }
+      });
+  }
+
+  ngOnInit() {
+    this.cargarRecursos();
+  }
+
 }
---
+++ frontend/front-portal-academico/src/app/services/api.spec.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private api = 'http://localhost:8000';
+
+  constructor(private http: HttpClient) {}
+
+  getResources() {
+    return this.http.get(`${this.api}/resources/`);
+  }
+
+  createUser(data: any) {
+    return this.http.post(`${this.api}/users/`, data);
+  }
+
+  createResource(data: any) {
+    return this.http.post(`${this.api}/resources/`, data);
+  }
+}
