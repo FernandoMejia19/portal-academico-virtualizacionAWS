a511e1c52e6e078cc7ee287e4b23efb662cffbd5
AngyUwU2005XD <angelicapanama2005@gmail.com>
2026-06-16 11:51:06 -0500

    Frontend Inicio

--- frontend/front-portal-academico/src/app/app.ts
+++ frontend/front-portal-academico/src/app/app.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ApiService } from './services/api.spec';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,7 @@ import { ApiService } from './services/api.spec';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
 
   mostrarFormulario = false;
 
@@ -32,7 +32,9 @@ export class App {
     user_id: 1
   };
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) {
+  console.log("CONSTRUCTOR");
+}
 
   crearUsuario() {
 
@@ -63,21 +65,62 @@ export class App {
       });
   }
 
-  cargarRecursos() {
+cargarRecursos() {
 
-    this.api.getResources()
-      .subscribe({
-        next: (data: any) => {
-          this.recursos = data;
-        },
-        error: (err: any) => {
-          console.error(err);
-        }
-      });
-  }
+  this.api.getResources()
+    .subscribe({
+      next: (data: any) => {
 
-  ngOnInit() {
-    this.cargarRecursos();
+        console.log("ANTES:", this.recursos.length);
+
+        this.recursos = [
+  {
+    id: 999,
+    title: 'PRUEBA MANUAL',
+    description: 'SI VES ESTO EL HTML FUNCIONA',
+    resource_type: 'DOCUMENT',
+    author: 'ANGELICA',
+    url: 'https://google.com'
   }
+];
+
+        console.log("DESPUES:", this.recursos.length);
+
+      },
+      error: (err: any) => {
+        console.error(err);
+      }
+    });
+
+}
+  obtenerImagen(tipo: string): string {
+
+  const imagenes: any = {
+
+    video:
+      'https://images.unsplash.com/photo-1515879218367-8466d910aaa4',
+
+    pdf:
+      'https://images.unsplash.com/photo-1516321318423-f06f85e504b3',
+
+    document:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40',
+
+    image:
+      'https://images.unsplash.com/photo-1498050108023-c5249f4df085',
+
+    curso:
+      'https://images.unsplash.com/photo-1517694712202-14dd9538aa97'
+
+  };
+
+  return imagenes[tipo?.toLowerCase()]
+    || 'https://images.unsplash.com/photo-1516321318423-f06f85e504b3';
+}
+
+ngOnInit() {
+  console.log("ENTRO AL INIT");
+  this.cargarRecursos();
+}
 
 }
