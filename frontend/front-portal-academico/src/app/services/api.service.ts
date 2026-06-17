2d0c9ece30b672280f5ad3d5eb099ffc99b9f293
FernandoMejia19 <fercholkm19@gmail.com>
2026-06-16 22:02:15 -0500

    Arreglo bugs en Front

--- frontend/front-portal-academico/src/app/services/api.service.ts
+++ frontend/front-portal-academico/src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
 
-  private api = 'http://3.224.64.55';
+  private api = '/api';
 
   constructor(private http: HttpClient) {}
   
